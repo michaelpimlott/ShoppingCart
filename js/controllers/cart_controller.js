96cbe2775a3410d347c981deaac2e6ac14a3d5a3
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-05-16 14:21:44 -0700

    fix button

--- js/controllers/cart_controller.js
+++ js/controllers/cart_controller.js
@@ -1,12 +1,12 @@
 app.controller('CartController', ['$scope', 'teaService', function ($scope, teaService) {
-    $scope.editSave = 'Edit'
+    $scope.SaveEdit = 'Edit'
     $scope.editQuantity = false;
     $scope.cart = teaService.getItems();
     $scope.orderTotal = teaService.orderTotal();
     $scope.toggleEdit = function () {
       this.editQuantity = !this.editQuantity;
       this.showQuantity = !this.showQuantity;
-      this.editSave = 'Save';
+      this.saveEdit = 'Save';
     }
     $scope.removeItem = function (item) {
       teaService.removeItem(item);
